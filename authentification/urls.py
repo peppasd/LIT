05dc140538fe253c55354c27875b25cfd4e86aeb
Kladis <53832177+dennisklad@users.noreply.github.com>
2020-06-04 15:27:56 +0200

    change dir

--- authentification/urls.py
+++ authentification/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.register, name='register-page'),
+    path('', views.signin, name='signin-page'),
+    path('', views.resetpsw, name='resetpsw-page'),
 ]
--- lit/settings.py
+++ lit/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentification',
-    'webpage.apps.WebpageConfig',
+    'authentification.apps.AuthentificationConfig',
 ]
 
 MIDDLEWARE = [
