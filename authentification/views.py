ce25918de70a2cd55986eddfaa8146feb48b0541
Deniz Furater <24463053+pozilei@users.noreply.github.com>
2020-06-05 14:42:25 +0200

    fixed typo

--- authentification/views.py
+++ authentification/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def register(request):
-    return render(request, 'fronend/register.html')
+    return render(request, 'frontend/html/register.html')
 
 def signin(request):
-    return render(request, 'fronend/sign-in.html')
+    return render(request, 'frontend/html/sign-in.html')
 
 def resetpsw(request):
-    return render(request, 'fronend/resetpsw.html')
+    return render(request, 'frontend/html/resetpsw.html')
