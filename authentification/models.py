d31ac850e8c7874934c2e06f750974edef64acd5
Deniz Furater <24463053+pozilei@users.noreply.github.com>
2020-06-05 18:04:50 +0200

    changed models.py: added primary-key field 'username' and assigned ordering to 'username' instead of 'lastname' + migrations

--- authentification/models.py
+++ authentification/models.py
@@ -2,16 +2,17 @@ from django.db import models
 
 
 # Create your models here.
-class User(models.Model):
+class UserProfile(models.Model):
     # Fields
+    username = models.CharField(max_length=20, help_text='Please enter your username.', primary_key=True)
     first_name = models.CharField(max_length=20, help_text='Please enter your firstname.')
     last_name = models.CharField(max_length=30, help_text='Please enter your lastname.')
-    email = models.CharField(max_length=50, help_text='Please enter your email address.')
+    email = models.CharField(max_length=25, help_text='Please enter your email address.')
     pwd = models.EmailField(max_length=256, help_text='Please enter your password.')
 
     #Metadata
     class Meta:
-        ordering = ['-last_name']
+        ordering = ['-username']
 
     # #Methods
     def __str__(self):
