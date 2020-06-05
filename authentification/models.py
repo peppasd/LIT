61a894ca5d65fdcc05b53fccb14e8bda0cfbd9ec
Deniz Furater <24463053+pozilei@users.noreply.github.com>
2020-06-05 16:20:44 +0200

    changed models.py + added migrations

--- authentification/models.py
+++ authentification/models.py
@@ -3,10 +3,16 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
-    pwd = models.CharField(max_length=256)
+    # Fields
+    first_name = models.CharField(max_length=20, help_text='Please enter your firstname.')
+    last_name = models.CharField(max_length=30, help_text='Please enter your lastname.')
+    email = models.CharField(max_length=50, help_text='Please enter your email address.')
+    pwd = models.EmailField(max_length=256, help_text='Please enter your password.')
 
+    #Metadata
+    class Meta:
+        ordering = ['-last_name']
+
+    # #Methods
     def __str__(self):
         return self.first_name + " " + self.last_name
