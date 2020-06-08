fc97bfb321379566cba484131351577b9e56e003
Aziz Misir <aziz.misir@campus.tu-berlin.de>
2020-06-08 16:20:34 +0200

    add profile_edit

--- authentification/forms.py
+++ authentification/forms.py
@@ -1,9 +1,10 @@
 from django.forms import EmailField
+from django import forms
 
 from django.utils.translation import ugettext_lazy as _
 
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
 class UserCreationFormExtended(UserCreationForm):
@@ -20,3 +21,34 @@ class UserCreationFormExtended(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class EditProfileForm(UserChangeForm):
+    password= None
+    email = forms.EmailField(
+        label=_("Email"),
+        max_length=254,
+        widget=forms.EmailInput(attrs={'autocomplete': 'email'})
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name'
+        )
+    
+    def save(self, commit=True):
+        user = super(UserChangeForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+class EditProfileFormInvalidEmail(EditProfileForm):
+    email = forms.EmailField(
+        label=_("Email"),
+        max_length=254,
+        widget=forms.EmailInput(attrs={'autocomplete': 'email'}),
+        help_text=_("<div class='alert alert-danger'><strong>Entered email was wrong, email was not changed, please try again!</strong></div>")
+    )
--- authentification/views.py
+++ authentification/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from .forms import UserCreationFormExtended
+from django.contrib.auth.forms import PasswordChangeForm, UserChangeForm
+from .forms import UserCreationFormExtended, EditProfileForm, EditProfileFormInvalidEmail
+from django.conf.urls import url
+from django.contrib.auth import update_session_auth_hash
+from django.urls import reverse
 
 # Create your views here.
 
@@ -24,3 +27,21 @@ def login(request):
 
 def logout(request):    
     return render(request, 'logout.html')
+
+def profile_edit(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            form = EditProfileFormInvalidEmail(instance=request.user)            
+            return render(request, 'registration/profile_edit.html', {
+            'form':form
+        }) 
+    else:
+        form = EditProfileForm(instance=request.user)
+        return render(request, 'registration/profile_edit.html', {
+            'form':form
+        })
