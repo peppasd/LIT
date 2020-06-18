69f7d55b0d23affee34f0daec70b797cb390aaf9
ElMagicoHD <48961123+ElMagicoHD@users.noreply.github.com>
2020-06-18 15:42:22 +0200

    fixed typo

--- lit/settings.py
+++ lit/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'lit.wsgi.application'
 
 def getDbHost(): 
     try:
-        if os.environ['GCR'] == true:
+        if os.environ['GCR'] == 'true':
             return '10.17.17.3'
         else:
             return 'db'
