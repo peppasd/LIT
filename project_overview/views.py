0110681c5250502d1b8d2da607a458d6d65349b0
Kladis <53832177+dennisklad@users.noreply.github.com>
2020-06-19 16:34:08 +0200

    need to add button on photos delete and checkmark

--- project_overview/views.py
+++ project_overview/views.py
@@ -30,10 +30,13 @@ tags = [
     'the queen',
     'old'
 ]
-photos = [
+images = [
     'https://www.cheatsheet.com/wp-content/uploads/2017/12/Queen-Elizabeth-Waving.jpg',
     'https://www.cheatsheet.com/wp-content/uploads/2017/10/queen-elizabeth-fur.jpg',
-    'http://i.dailymail.co.uk/i/newpix/2018/06/29/21/4DBC9DE400000578-0-Fans_of_the_Royal_family_grew_worried_about_Queen_Elizabeth_II_s-m-55_1530303342340.jpg'
+    'http://i.dailymail.co.uk/i/newpix/2018/06/29/21/4DBC9DE400000578-0-Fans_of_the_Royal_family_grew_worried_about_Queen_Elizabeth_II_s-m-55_1530303342340.jpg',
+    'https://tse3.mm.bing.net/th?id=OIP.raEAx7WtuS2iOyrYhTQUdQHaE8&pid=Api',
+    'https://tse4.mm.bing.net/th?id=OIP.kiRqPNokgSnJ6ApWGksB2QHaJe&pid=Api',
+    'https://tse1.mm.bing.net/th?id=OIP.HC7u7SXAjRSapw29hIQ6ZgHaFj&pid=Api'
 ]
 
 # Create your views here.
@@ -41,7 +44,7 @@ def project_overview(request):
     context = {
         'projects': projects,
         'members': members,
-        'photos':photos,
+        'images': [images[0],images[1]],
         'tags':tags
     }
     #project = get_object_or_404(Project, pk=project_id)
@@ -52,7 +55,7 @@ def project_images(request):
     context = {
         'projects': projects,
         'members': members,
-        'photos':photos,
+        'images':images,
         'tags':tags
     }
     return render(request, 'project_images.html', context)
