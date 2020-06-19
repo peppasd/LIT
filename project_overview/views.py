aaee81281f372d85fc7fed7798147ee5ec59dd79
Kladis <53832177+dennisklad@users.noreply.github.com>
2020-06-19 21:17:08 +0200

    both static pages done
    with trash icon and tagged T/F indicator
    
    //TODO
    Cant scroll until the end sometimes.

--- project_overview/views.py
+++ project_overview/views.py
@@ -31,13 +31,21 @@ tags = [
     'old'
 ]
 images = [
-    'https://www.cheatsheet.com/wp-content/uploads/2017/12/Queen-Elizabeth-Waving.jpg',
-    'https://www.cheatsheet.com/wp-content/uploads/2017/10/queen-elizabeth-fur.jpg',
-    'http://i.dailymail.co.uk/i/newpix/2018/06/29/21/4DBC9DE400000578-0-Fans_of_the_Royal_family_grew_worried_about_Queen_Elizabeth_II_s-m-55_1530303342340.jpg',
-    'https://tse3.mm.bing.net/th?id=OIP.raEAx7WtuS2iOyrYhTQUdQHaE8&pid=Api',
-    'https://tse4.mm.bing.net/th?id=OIP.kiRqPNokgSnJ6ApWGksB2QHaJe&pid=Api',
-    'https://tse1.mm.bing.net/th?id=OIP.HC7u7SXAjRSapw29hIQ6ZgHaFj&pid=Api'
+    {'url':'https://www.cheatsheet.com/wp-content/uploads/2017/12/Queen-Elizabeth-Waving.jpg',
+     'isTagged': True,},
+    {'url':'https://www.cheatsheet.com/wp-content/uploads/2017/10/queen-elizabeth-fur.jpg',
+    'isTagged': False,},
+    {'url':'http://i.dailymail.co.uk/i/newpix/2018/06/29/21/4DBC9DE400000578-0-Fans_of_the_Royal_family_grew_worried_about_Queen_Elizabeth_II_s-m-55_1530303342340.jpg',
+    'isTagged': False,},
+    {'url':'https://tse3.mm.bing.net/th?id=OIP.raEAx7WtuS2iOyrYhTQUdQHaE8&pid=Api',
+    'isTagged':True,},
+    {'url':'https://tse4.mm.bing.net/th?id=OIP.kiRqPNokgSnJ6ApWGksB2QHaJe&pid=Api',
+    'isTagged':True},
+    {'url':'https://tse1.mm.bing.net/th?id=OIP.HC7u7SXAjRSapw29hIQ6ZgHaFj&pid=Api',
+    'isTagged':True,},
 ]
+count_images = images.__len__
+tagged_images = 4
 
 # Create your views here.
 def project_overview(request):
@@ -45,7 +53,8 @@ def project_overview(request):
         'projects': projects,
         'members': members,
         'images': [images[0],images[1]],
-        'tags':tags
+        'tags':tags,
+        'count_images': count_images,
     }
     #project = get_object_or_404(Project, pk=project_id)
     return render(request, 'project_overview.html', context)
