f84f92b5b57a49da8d79b3d42c7499a01e10deda
Kladis <53832177+dennisklad@users.noreply.github.com>
2020-06-19 15:00:47 +0200

    Page overview done with dummy

--- project_overview/urls.py
+++ project_overview/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.project_overview, name='project_overview'),
-  #  path('pictures/', views.about, name='pictures')
+  path('', views.project_overview, name='project_overview'),
+#  path('<int:project_id>', views.project_overview, name='project_overview'),
+
+  path('images/', views.project_images, name='project_images')
 ]
--- project_overview/views.py
+++ project_overview/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 #Dummy Project
 projects = [
@@ -25,12 +25,35 @@ members = [
        'username': 'Raw John'
     }
 ]
-
+tags = [
+    'testing',
+    'the queen',
+    'old'
+]
+photos = [
+    'https://www.cheatsheet.com/wp-content/uploads/2017/12/Queen-Elizabeth-Waving.jpg',
+    'https://www.cheatsheet.com/wp-content/uploads/2017/10/queen-elizabeth-fur.jpg',
+    'http://i.dailymail.co.uk/i/newpix/2018/06/29/21/4DBC9DE400000578-0-Fans_of_the_Royal_family_grew_worried_about_Queen_Elizabeth_II_s-m-55_1530303342340.jpg'
+]
 
 # Create your views here.
 def project_overview(request):
     context = {
         'projects': projects,
-        'members': members
+        'members': members,
+        'photos':photos,
+        'tags':tags
     }
+    #project = get_object_or_404(Project, pk=project_id)
     return render(request, 'project_overview.html', context)
+
+
+def project_images(request):
+    context = {
+        'projects': projects,
+        'members': members,
+        'photos':photos,
+        'tags':tags
+    }
+    return render(request, 'project_images.html', context)
+
