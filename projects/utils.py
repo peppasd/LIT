2000d149d939657f37d9f5ef4bbfdb299cd8dd53
Aziz Misir <aziz.misir@campus.tu-berlin.de>
2020-06-20 21:04:52 +0200

    add allProject_user

--- projects/utils.py
+++ projects/utils.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import User
+from .models import Project
 
 def existsUser(username):
     if User.objects.filter(username=username).exists():
         return True
     
     return False
+
+def allProjects_user(username):
+    ret = []
+    users = Project.users.objects.all()
+    for elm in users:
+        if elm.username == username:
+            ret.append(elm)
+    return ret
