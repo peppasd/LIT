344526b0b988671d3a9209b1b7470d16c894d948
Kladis <53832177+dennisklad@users.noreply.github.com>
2020-07-05 19:40:54 +0200

    iteratable table and images

--- carousel/views.py
+++ carousel/views.py
@@ -2,5 +2,37 @@ from django.shortcuts import render
 
 # Create your views here.
 
+labelimages = [
+    {
+    "url": "https://www.wonderplugin.com/wp-content/uploads/2013/12/Evening_1024.jpg",
+    "Band": "false",
+    "Queen": "true",
+    "Age":  "130",
+    },
+    {
+    "url": "https://www.wonderplugin.com/wp-content/uploads/2013/12/Evening_1024.jpg",
+    "Band": "false",
+    "Queen": "true",
+    "Age":  "n/d",
+    },
+
+    {
+    'url': "https://www.wonderplugin.com/wp-content/uploads/2013/12/Evening_1024.jpg",
+    "Band": "true",
+    "Queen": "false",
+    "Age":  90,
+    },
+]
+tags = [
+    "Band",
+    "Queen",
+    "Age",
+]
+
+
 def project_carousel(request):
-    return render(request, 'project_carousel.html')
+    context = {
+        'labelimages': labelimages,
+        'tags': tags
+    }
+    return render(request, 'project_carousel.html', context)
