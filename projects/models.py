1117bef3df24424d6539eda4d4e628c8b76ec62b
Deniz Furater <24463053+pozilei@users.noreply.github.com>
2020-07-06 15:34:10 +0200

    dyn project ov excluding ‘images‘ and 'count_images'; #45 made all projects public; #46

--- projects/models.py
+++ projects/models.py
@@ -4,21 +4,23 @@ import datetime
 import uuid
 from django.dispatch import receiver
 
+
 # Create your models here.
 
 class Project(models.Model):
     name = models.CharField(max_length=160)
     description = models.CharField(max_length=160)
-    progress = models.IntegerField(default=0)    
+    progress = models.IntegerField(default=0)
     created = models.DateField(default=datetime.date.today)
     labeled_photos = models.IntegerField(default=0)
     photos_total = models.IntegerField(default=0)
-    labels=models.CharField(max_length=160, default='',blank=True)
-    user_tmp=models.CharField(max_length=1024, default='',blank=True)
+    labels = models.CharField(max_length=160, default='', blank=True)
+    user_tmp = models.CharField(max_length=1024, default='', blank=True)
     users = models.ManyToManyField(User)
 
     def __str__(self):
         return self.name
+
 
 class Photo(models.Model):
     uuid = models.UUIDField(
@@ -30,7 +32,7 @@ class Photo(models.Model):
     project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
 
 
-#remove photo from s3 as well
+# remove photo from s3 as well
 @receiver(models.signals.post_delete, sender=Photo)
 def remove_file_from_bucket(sender, instance, using, **kwargs):
     instance.photo.delete(save=False)
