cebcd6ec6d4df0590d9dbad77afdb147d7c2886e
Mateusz Andrzej Musial <mateusz.musial@interia.eu>
2020-07-10 14:28:53 +0200

    tag model

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,6 @@ from django.dispatch import receiver
 
 
 # Create your models here.
-
 class Project(models.Model):
     name = models.CharField(max_length=160)
     description = models.CharField(max_length=160)
@@ -17,10 +16,17 @@ class Project(models.Model):
     labels = models.CharField(max_length=160, default='', blank=True)
     user_tmp = models.CharField(max_length=1024, default='', blank=True)
     users = models.ManyToManyField(User)
+    ownerusers = models.ManyToManyField(User)
 
     def __str__(self):
         return self.name
 
+#add  isTagged boolean set it to method call that checks if given photo instance has a tag
+
+class Tag(models.Model):
+    InputTagName = models.CharField(max_length=160)
+    inputState = models.CharField(max_length=160)
+    project = models.ForeignKey(Project, related_name='tags', on_delete=models.CASCADE)
 
 class Photo(models.Model):
     uuid = models.UUIDField(
@@ -30,6 +36,12 @@ class Photo(models.Model):
     title = models.CharField(max_length=100)
     photo = models.FileField()
     project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    def isTagged(self):
+        if len(self.tag_set.all())>0:
+            return True
+        return False
+
 
 
 # remove photo from s3 as well
