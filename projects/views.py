ce139921b5785efe16489ef06a1a678701c3ed2f
Aziz Misir <aziz.misir@campus.tu-berlin.de>
2020-07-21 19:15:24 +0200

    warning user does not exist by invite

--- projects/views.py
+++ projects/views.py
@@ -9,12 +9,13 @@ from django.contrib.auth.decorators import login_required
 from django.core.files.storage import FileSystemStorage
 
 
+
 # Create your views here.
 
 @login_required
 def overview(request):
     project_list = []
-    all_projects = []
+    all_projects = []   
     for project in Project.objects.all():
         for member in project.members.all():
             if request.user == member.user:
@@ -45,45 +46,49 @@ def new_project(request):
             post.created = str
             post.save()
             post.owners.add(request.user)
-            post.save()
+            post.save()            
             return HttpResponseRedirect('/projects/')
     else:
         form = ProjectForm()
 
     return render(request, 'new_project.html', {'form': form})
 
-
 @login_required
-def project_overview(request, pk):
+def project_overview(request,pk):    
     projects = []
     members = []
     tags = []
     images = []
+    ph = ""
+
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
         username = request.POST['username']
-        if username == '__join__':
+        if username=='__join__':
             m = Member(user=request.user)
             m.save()
             project.members.add(m)
         else:
             if existsUser(username):
                 project.owners.add(getUser(username))
-
+            else:
+                ph = "User {} does not exist".format(username)
+            
     members = allUsers_project(project)
     tags = allTags_project(project)
     imgs = project.images.all()
     for img in imgs:
-        images.append({'url': img.url})
+        images.append({'url':img.url})
     count_images = images.__len__
     tagged_images = 4
 
     context = {
-        'project': project,
+        'project':project,        
         'members': members,
         'images': images,
         'tags': tags,
         'count_images': count_images,
+        'ph' : ph,
     }
     # project = get_object_or_404(Project, pk=project_id)
     return render(request, 'project_overview.html', context)
@@ -100,11 +105,11 @@ def project_images(request, pk):
     tags = allTags_project(project)
     imgs = project.images.all()
     for img in imgs:
-        images.append({'url': img.url})
+        images.append({'url':img.url})
     count_images = images.__len__
     tagged_images = 4
     context = {
-        'project': project,
+        'project':project,        
         'members': members,
         'images': images,
         'tags': tags,
@@ -124,45 +129,44 @@ VALID_IMAGE_EXTENSIONS = [
 @login_required
 def upload_images(request, pk):
     context = {
-        'pk': pk,
-    }
-
+            'pk': pk,            
+        } 
+            
     if request.method == "POST":
         uploaded_files = request.FILES.getlist('img')
         for uploaded_file in uploaded_files:
             ends = uploaded_file.name.split(".")
-            end = ends[1]
+            end = ends[1]           
             if VALID_IMAGE_EXTENSIONS.count(end) > 0:
                 fs = FileSystemStorage()
                 name = fs.save(uploaded_file.name, uploaded_file)
                 url = fs.url(name)
                 now = datetime.datetime.now()
                 str = now.strftime('%Y-%m-%d')
-                obj = Photo(created_at=str, title=name, name=name, url=url, project=Project.objects.get(id=pk))
+                obj = Photo(created_at=str,title=name,name=name,url=url,project=Project.objects.get(id=pk))
                 obj.save()
     return render(request, 'upload_images.html', context)
 
-
 @login_required
-def create_tags(request, pk):
+def create_tags(request, pk): 
     context = {
-        'pk': pk,
-    }
+            'pk': pk,            
+        } 
     if request.method == "POST":
-        form = LabelForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
+        form = LabelForm(request.POST)       
+        if form.is_valid():            
+            post = form.save(commit=False)                        
             post.save()
-            post.project = Project.objects.get(id=pk)
+            post.project = Project.objects.get(id=pk) 
             post.save()
-            txt = '/projects/test/{}/'.format(pk)
+            txt = '/projects/test/{}/'.format(pk)                     
             return HttpResponseRedirect(txt)
     else:
         form = LabelForm()
         context = {
             'pk': pk,
             'form': form,
-        }
-    return render(request, 'create_tags.html', context)
+        }        
+    return render(request, 'create_tags.html', context) 
 
-
+    
