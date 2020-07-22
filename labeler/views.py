6215dabe2a0f60276087ba18b2d2498b23d05e6f
Aziz Misir <aziz.misir@campus.tu-berlin.de>
2020-07-22 14:08:00 +0200

    #70 bugfix load current Photo

--- labeler/views.py
+++ labeler/views.py
@@ -3,19 +3,34 @@ from projects.utils import allTags_project
 from projects.models import Project, Value, Label, Photo
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
-def project_labeler(request, pk):
+def project_labeler(request, pk):    
     labelimages = []
     tagLists = []
     project = Project.objects.get(id=pk)
-    labelimages = project.images.all()
+
+    for img in project.images.all():
+        labelimages.append(img)
+     
     tagLists = Label.objects.all()
+    loopId = 0    
+    labelimages = list(labelimages)    
 
     if request.method == 'POST':
         tagIds = list(request.POST.getlist('tagId'))
         tagVals = request.POST.getlist('tagVal')
         addId = request.POST['addTag']
         imgId = request.POST['imgId']
+        loopId = request.POST['loopId']
+        loopId = int(loopId)
+        if loopId != 0:
+            indexImg = 0
+            for i,j in enumerate(labelimages):               
+                if str(j.uuid) == imgId:
+                    indexImg = i
+                    break
+            labelimages[0], labelimages[indexImg] = labelimages[indexImg], labelimages[0]
 
         if addId:
             photo = Photo.objects.get(uuid=imgId)
@@ -29,7 +44,7 @@ def project_labeler(request, pk):
                 valObj.label.add(getLabel)
                 valObj.save()
                 valObj.photo = photo
-                valObj.save()
+                valObj.save()        
 
         for i in range(len(tagIds)):
             obj = Value.objects.get(id=tagIds[i])
